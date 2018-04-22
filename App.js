f9203aec487b29a3a3566e44b722b16ac496c3ea
Alice Borges <aliceborges@outlook.com.br>
2018-04-22 16:47:36 -0300

    Continuação Align Items: Stretch (Verticalmente)
    const styles = StyleSheet.create({
      container: {
        flex: 1,
        alignItems: 'stretch',
        flexDirection: 'row',
      },
      box: {
        width: 50,
        backgroundColor: '#e76e63',
        margin: 10,
      }
    })
    Note que alterei a flexDirection para row, e adicionei width: 50, além de remover height: 50.
    
    flex-direction: row e alignItems: stretch faz com que os flex items estiquem verticalmente ao longo do Cross Axis.
    Vamos entender isso. Primeiro, o Main Axis é agora horizontal, pois adicionamos flexDirection: row. Isso significa que alignItems vai alinhar os itens ao longo do eixo vertical . Como removemos a altura dos elementos-filhos e adicionamos alignItems: stretch, esses elementos irão esticar ao longo do eixo vertical, ocupando toda a largura do elemento-pai que, nesse caso, é a tela inteira.

--- App.js
+++ App.js
@@ -16,12 +16,12 @@ class FlexboxExamples extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    //flexDirection: 'row',
+    flexDirection: 'row',
     alignItems: 'stretch',
   },
   box: {
-    height: 50,
-    //width: 50,
+    //height: 50,
+    width: 50,
     backgroundColor: '#e76e63',
     margin: 10,
   }
